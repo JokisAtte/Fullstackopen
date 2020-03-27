4336316249c53dc2fb5fc1732885ca4b42e894d0
JokisAtte <atte.jokinen@tuni.fi>
2020-03-27 19:27:21 +0200

    Tehtävä 1.8

--- part1/1.6/src/index.js
+++ part1/1.6/src/index.js
@@ -2,7 +2,22 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Statistics = (props) => {
+  const good = props.good
+  const neutral = props.neutral
+  const bad = props.bad
+  const totalVotes = good + neutral + bad
 
+  return(
+  <div>
+    <h1>statistics</h1>
+    <p> good {good}</p>
+    <p> neutral {neutral}</p>
+    <p> bad {bad}</p>
+    <p> all {(totalVotes)} </p>
+    <p> average {(good+bad*-1)/(totalVotes)} </p>
+    <p> positive {good/(totalVotes)*100} %</p>
+  </div>
+  )
 }
 
 const App = () => {
@@ -10,7 +25,6 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const totalVotes = good + neutral + bad
 
   return (
     <div>
@@ -18,13 +32,7 @@ const App = () => {
       <button onClick = {() => setGood(good +1)}>good</button>
       <button onClick = {() => setNeutral(neutral +1)}>neutral</button>
       <button onClick = {() => setBad(bad +1)}>bad</button>
-      <h1>statistics</h1>
-      <p> good {good}</p>
-      <p> neutral {neutral}</p>
-      <p> bad {bad}</p>
-      <p> all {(totalVotes)} </p>
-      <p> average {(good+bad*-1)/(totalVotes)} </p>
-      <p> positive {good/(totalVotes)*100} %</p>
+      <Statistics good = {good} neutral = {neutral} bad ={bad}/>
     </div>
   )
 }
