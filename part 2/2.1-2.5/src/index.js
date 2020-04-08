31e061a7cba1111f36fd8bfad265fadaefe7b0f0
JokisAtte <atte.jokinen@tuni.fi>
2020-04-08 11:18:24 +0300

    tehtävän 2.4 palautus

--- part 2/2.1-2.5/src/index.js
+++ part 2/2.1-2.5/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 const Header = ({ course }) => {
   return (
-    <h1>{course.name}</h1>
+    <h2>{course.name}</h2>
   )
 }
 
@@ -11,7 +11,7 @@ const Total = ({ course }) => {
   const parts = course.parts
   var totalParts = parts.reduce((sum,part) => sum + part.exercises, 0)
   return(
-    <p>Number of exercises {totalParts}</p>
+    <b>Number of exercises {totalParts}</b>
   ) 
 }
 
@@ -45,31 +45,57 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course = {course}></Course>
+      <h1>Web development curriculum</h1>
+      {courses.map((course,i) =>
+        <Course key = {i} course = {course}/>
+      )}
     </div>
   )
 }
