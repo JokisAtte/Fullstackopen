baf73dccfaacf419d45b3f7deb1fe1e63d0f894a
JokisAtte <atte.jokinen@tuni.fi>
2020-04-09 11:22:26 +0300

    2.6 palautus

--- part 2/testit/materiaalit/src/App.js
+++ part 2/testit/materiaalit/src/App.js
@@ -1,17 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
-    return (
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random > 0.5,
+      id: notes.lenght +1,
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteCHange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
       <div>
-        <h1>Notes</h1>
-        <ul>
-            {notes.map((note,i) =>
-              <Note key={i} note={note}/>
-            )}
-        </ul>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
       </div>
-    )
+      <ul>
+          {notesToShow.map((note,i) =>
+            <Note key={i} note={note}/>
+          )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value = {newNote}
+          onChange ={handleNoteCHange}
+        />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
   }
 
 export default App
