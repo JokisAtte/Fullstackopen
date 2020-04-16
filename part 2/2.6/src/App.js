47ab24e15acfeb552dc3b7fe35f2c35950be8a99
JokisAtte <atte.jokinen@tuni.fi>
2020-04-16 14:49:05 +0300

    2.11 palautus

--- part 2/2.6/src/App.js
+++ part 2/2.6/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = (props) => {
     return(
@@ -27,15 +28,23 @@ const FilterForm =(props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456' },
-    { name: 'Ada Lovelace', phone: '39-44-5323523' },
-    { name: 'Dan Abramov', phone: '12-43-234345' },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName ] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [newSearch, setNewSearch] = useState('')
+
+  const promise = axios.get('http://localhost:3001/persons')
+  console.log(promise)
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  },[])
+
 
   const addName = (event) => {
     event.preventDefault()
