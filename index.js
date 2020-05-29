11514da07931233655a6402d51037fc333151b2a
JokisAtte <atte.jokinen@tuni.fi>
2020-05-29 09:50:20 +0300

    Kovakoodattuja yhteystietoja tarjoava palvelin toteutettu

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello World')
+let persons = [
+    {
+      "name": "Kalle",
+      "number": "123",
+      "id": 1
+    },
+    {
+      "name": "Matti",
+      "number": "123",
+      "id": 2
+    }
+]
+
+app.get('/', (reg,res) => {
+  res.send('<h1> Hello! </h1>')
+})
+
+app.get('/api/persons', (req,res) => {
+  res.json(persons)
 })
 
 const port = 3001
