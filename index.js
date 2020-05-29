0160e823a4e21b62cca0e98f19015667a0f116eb
JokisAtte <atte.jokinen@tuni.fi>
2020-05-29 10:42:31 +0300

    Lisätty toiminnallisuus yksittäisen yhteystiedon näyttämiseen

--- index.js
+++ index.js
@@ -19,13 +19,20 @@ app.get('/', (reg,res) => {
   res.send('<h1> Hello! </h1>')
 })
 
-app.get('/api/persons', (req,res) => {
+app.get('/api/persons/', (req,res) => {
   res.json(persons)
 })
 
+app.get('/api/persons/:id', (req,res) => {
+  const id = req.params.id
+  const person = persons.find(per => per.id == id)
+  res.json(person)
+})
+
 app.get('/info', (reg,res) => {
+  console.log(reg.query)
   let today = new Date()
-  let date = `${today.getHours()}:${today.getMinutes()} ${today.getDate()} - ${today.getMonth() + 1 } - ${today.getFullYear()}`
+  let date = `${today.getHours()}:${today.getMinutes()} ${today.getDate()}.${today.getMonth() + 1 }.${today.getFullYear()}`
   res.send(`<div> Phonebook has info for ${persons.length} people </div> ${date}`)
 })
 
