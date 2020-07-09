34d30d07bf5fc2106192a912fcd8e8a1b7f380ef
JokisAtte <atte.jokinen@tuni.fi>
2020-07-09 15:45:19 +0300

    Palvelin ja frontend valmiina, lisätty V1.0 toiminnot

--- Bloglistapp/index.js
+++ Bloglistapp/index.js
@@ -1,34 +1,13 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const Blog = require('./models/blog')
+//Käynnistää palvelimen
+const app = require('./app') // varsinainen Express-sovellus
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-require('dotenv').config()
-
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+const server = http.createServer(app)
 
 const PORT = process.env.PORT || 3003
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`)
 })
--- Bloglistapp/models/blog.js
+++ Bloglistapp/models/blog.js
@@ -1,23 +1,19 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const mongoUrl = process.env.MONGODB_URI
-console.log(process.env)
-console.log("connecting...")
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
-  .then( result => {
-    console.log("Connected to mongoDB")
-  })
-  .catch( error => {
-    console.log("Connection to MongoDB failed")
-    console.log(error.message)
-  })
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
 
-  const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-  })
-  
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 module.exports = mongoose.model('Blog', blogSchema)
