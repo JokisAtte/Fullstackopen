ec8b34de3beb449b0bc00681814b26bd0ec5081a
JokisAtte <atte.jokinen@tuni.fi>
2020-07-30 10:36:05 +0300

    ohjelman toiminnallisuus valmis

--- unicafe-redux/src/reducer.js
+++ unicafe-redux/src/reducer.js
@@ -6,14 +6,19 @@ const initialState = {
 
 const counterReducer = (state = initialState, action) => {
   console.log(action)
+  console.log(state)
   switch (action.type) {
     case 'GOOD':
+      state = {...state, good: state.good +1}
       return state
     case 'OK':
+      state = {...state, ok: state.ok +1}
       return state
     case 'BAD':
+      state = {...state, bad: state.bad +1}
       return state
     case 'ZERO':
+      state = initialState
       return state
     default: return state
   }
